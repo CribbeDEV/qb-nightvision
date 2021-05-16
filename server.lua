298a5f5a3fc3fa81f8613874599f2958d8196cf6
OsmiumOP <71386574+OsmiumOP@users.noreply.github.com>
2021-05-16 13:44:00 +0530

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,6 +1,10 @@
+  
 QBCore = nil
 TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 
 QBCore.Functions.CreateUseableItem("nightvision", function(source)
-    TriggerClientEvent("qb-nightvision:toggle", source)
+    local player = QBCore.Functions.GetPlayer(source)
+        if player ~= nil then 
+            TriggerClientEvent("qb-nightvision:toggle", source)
+        end 
 end)
