049ebebd42cc504952bbabb19872c75d29f6bb77
CribbeDEV <50823972+CribbeDEV@users.noreply.github.com>
2021-05-24 21:04:10 +0200

    Fixed issues
    Fixed issues that appeared after last pull request

--- client.lua
+++ client.lua
@@ -1,3 +1,4 @@
+nv = false
 QBCore = nil
 
 Citizen.CreateThread(function() 
@@ -10,20 +11,20 @@ end)
 RegisterNetEvent('qb-nightvision:toggle')
 AddEventHandler('qb-nightvision:toggle', function()
 	local ChangeClothes = true -- Change this to false if you don't want the mask to appear on the players character
-	local ped = PlayerPedId()
+	local ped = GetPlayerPed(-1)
 		if nv == false then
 			nv = true
 			TriggerServerEvent("InteractSound_SV:PlayOnSource", "nv", 0.25)
 			SetNightvision(true)
 			if ChangeClothes == true then
-				TaskPlayAnim(PlayerPedId(), "mp_masks@standard_car@ds@", "put_on_mask", 2.0, 2.0, 800, 51, 0, false, false, false)
+				TaskPlayAnim(ped, "mp_masks@standard_car@ds@", "put_on_mask", 2.0, 2.0, 800, 51, 0, false, false, false)
 				SetPedComponentVariation(ped, 1, 132, 0, 0)
 			end
 		elseif nv == true then
 			SetNightvision(false)
 			nv = false
 				if ChangeClothes == true then
-				TaskPlayAnim(PlayerPedId(), "mp_masks@standard_car@ds@", "put_on_mask", 2.0, 2.0, 800, 51, 0, false, false, false)
+				TaskPlayAnim(ped, "mp_masks@standard_car@ds@", "put_on_mask", 2.0, 2.0, 800, 51, 0, false, false, false)
 				SetPedComponentVariation(ped, 1, 0, 0, 0)
 				end
 end
