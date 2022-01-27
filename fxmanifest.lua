1934bec22b071e7be22e144cccfeb9e0a1a1caf8
izMystic <mystic@izmystic.dev>
2022-01-27 00:13:20 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,16 +7,3 @@ version '1.0.0'
 
 client_script 'client/main.lua'
 server_script 'server/main.lua'
-
-["nightvision"] = {
-    ["name"] = "nightvision",
-    ["label"] = "Night Vision goggles",
-    ["weight"] = 5,
-    ["type"] = "item",
-    ["image"] = "nightvision.png",
-    ["unique"] = true,
-    ["useable"] = true,
-    ["shouldClose"] = true,
-    ["combinable"] = nil,
-    ["description"] = "Bravo Six, going dark"
-}
